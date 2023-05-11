2157effc3709e622fa7c566edd85da18152fb567
asadmalli <asadmaqsoodmalli@gmail.com>
2023-05-11 09:10:09 +0200

    1. Kapselung von Codeblöcken

--- script_01.js
+++ script_01.js
@@ -3,3 +3,16 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf (call)
+test();
+
+// Funktionsrumpf (body) l callee
+// Funktionsdeklaration
+function test() 
+{
+    console.log("Hallo asad!");
+}
+
+test();
+
+
